af125033b3e2ab5e3772f0632b704f85a476128c
Colby Eastmond <colby.eastmond@gmail.com>
2025-02-20 10:27:56 -0700

    Created a function to multiply two numbers

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Colby Eastmond
+# 2/20/2025
+# Take input and multiply it
+
+# Takes two numbers and returns the product
+def multiply_two_numbers(num1, num2):
+    result = num1 * num2
+    return result
